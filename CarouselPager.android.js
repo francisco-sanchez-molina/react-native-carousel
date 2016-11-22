2b9f2f2ea1679a1b8ad58ada91657401ec8beed9
Akos Szoke <akos.szoke@possible.com>
2016-11-22 16:43:59 +0100

    Android fix vertical paging

--- CarouselPager.android.js
+++ CarouselPager.android.js
@@ -1,42 +1,81 @@
 var React = require('react');
 var {
-  View,
-  ViewPagerAndroid,
+  ScrollView,
 } = require('react-native');
 
+const slideOffset = 0.10;
+
 var CarouselPager = React.createClass({
+  currentPage: 0,
+
+  componentDidMount() {
+    setTimeout(() => {
+      // this.refs.scrollView.scrollTo(300);
+    }, 1500);
+  },
 
   scrollToPage(page, animated) {
     if (typeof animated === 'undefined') {
       animated = true;
     }
-    if (animated) {
-      this.refs.viewPager.setPage(page);
+
+    if (this.props.isHorizontal) {
+      this.refs.scrollView.scrollTo({x: page * this.props.width, y: 0, animated: animated});
     } else {
-      this.refs.viewPager.setPageWithoutAnimation(page);
+      this.refs.scrollView.scrollTo({y: page * this.props.height, x: 0, animated: animated});
     }
-    this._onPageSelected(page);
   },
 
-  _onPageSelected(page) {
-    this.props.onEnd(page);
+  adjustPageOffset(activePage) {
+    let nextPage = this.currentPage;
+    let toAnimate = true;
+
+    if (this.currentPage < activePage &&
+      (this.currentPage + slideOffset) < activePage) {
+      nextPage = this.currentPage + 1;
+      toAnimate = (nextPage + 1) > activePage;
+
+    } else if (this.currentPage > activePage &&
+      (this.currentPage - slideOffset) > activePage) {
+        nextPage = this.currentPage - 1;
+        toAnimate = (nextPage - 1) < activePage;
+    }
+
+    return {
+      nextPage,
+      toAnimate,
+    };
+  },
+
+  _onMomentumScrollEnd(e) {
+    if (this.props.isHorizontal) {
+      var activePage = e.nativeEvent.contentOffset.x / this.props.width;
+    } else {
+      var activePage = e.nativeEvent.contentOffset.y / this.props.height;
+    }
+    const adjusted = this.adjustPageOffset(activePage);
+    this.currentPage = adjusted.nextPage;
+    this.props.onEnd(this.currentPage);
+    this.scrollToPage(this.currentPage, adjusted.toAnimate);
   },
 
   render() {
-    return <ViewPagerAndroid
-      ref={(viewPager) => this.refs = { viewPager }}
-      style={{flex: 1}}
+    return <ScrollView
+      ref={(scrollView) => this.refs = { scrollView }}
       contentContainerStyle={this.props.contentContainerStyle}
       automaticallyAdjustContentInsets={false}
       horizontal={this.props.isHorizontal}
+      pagingEnabled={true}
       showsHorizontalScrollIndicator={false}
-      bounces={false}
-      onPageScroll={this.props.onBegin}
-      onPageSelected={(e) => this._onPageSelected(e.nativeEvent.position)}
+      showsVerticalScrollIndicator={false}
+      bounces={true}
+      onTouchStart={this.props.onBegin}
       scrollsToTop={false}
-      >
-        {this.props.children.map((c, idx) => <View key={idx} style={{flex: 1}}>{c}</View>)}
-      </ViewPagerAndroid>;
+      style={{ height: this.props.height, width: this.props.width, backgroundColor: 'transparent' }}
+      onScrollEndDrag={this._onMomentumScrollEnd}
+    >
+      {this.props.children}
+    </ScrollView>;
   },
 });
 
